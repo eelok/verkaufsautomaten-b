52b097bbef6f16fecf7ff4d116a68d78b39d51a0
eelok <eelok@mail.ru>
2020-01-11 16:48:58 +0100

    refactor method into Transit class

--- src/main/java/automat/net/ServerAutomat.java
+++ src/main/java/automat/net/ServerAutomat.java
@@ -17,59 +17,17 @@ import java.util.List;
 
 public class ServerAutomat extends IOException {
 
-    private static Automat automatInServer = new Automat(5);
-    private static KuchenParser kuchenParser = new KuchenParser();
-    private static Command existingCommand;
-
-    public static String existingCommand(String inputCommand, String inputData) throws IOException {
-        Command userCommand;
-        try {
-            userCommand = Command.valueOf(inputCommand);
-        } catch (IllegalArgumentException ex){
-            return String.format("from server: {%s} wrong command", inputCommand);
-        }
-        switch (userCommand){
-            case q:
-                break;
-            case addH:
-                if(automatInServer.addHersteller(new HerstellerImplementation(inputData.toLowerCase()))){
-                    return String.format("from server: hersteller %s was added to automat", inputData);
-                }
-            case addK:
-                Kuchen kuchenInfo = kuchenParser.getKuchenInfo(inputData);
-                automatInServer.addKuchen(kuchenInfo, LocalDateTime.now());
-                return String.format("from server: kuchen %s was added to automat", kuchenInfo.getType());
-            case listH:
-                List<Hersteller> herstellerList = automatInServer.getHerstellerList();
-                return "from server: " + herstellerList.toString();
-            case listK:
-                List<Kuchen> allEingelagertenKuchen = automatInServer.getAllEingelagertenKuchen();
-                return "from server: " + allEingelagertenKuchen.toString();
-            case delH:
-                automatInServer.deleteHersteller(inputData);
-                return String.format("from server: harsteller %s was deleted from automat", inputData);
-            case delK:
-                String typeOfKuchenWasDel = deleteKuchen(inputData);
-                return String.format("from server: %s from fach %s was deleted", typeOfKuchenWasDel, inputData);
-        }
-        return null;
-    }
-    private static String deleteKuchen(String inputData){
-        String number = inputData.replace("f", "");
-        int fachNum = Integer.parseInt(number);
-        EinlagerungEntry einlagerungEntry = automatInServer.removeKuchenFromAutomat(fachNum);
-        return einlagerungEntry.getKuchen().getType();
-    }
-
-
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
+        Automat automatInServer = new Automat(5);
+        KuchenParser kuchenParser = new KuchenParser();
+
+        Transit transit = new Transit(automatInServer, kuchenParser);
         int port = 1234;
         ServerSocket serverSocket = new ServerSocket(port);
         Socket socket = serverSocket.accept();
         ObjectInputStream serverInputStream = new ObjectInputStream(socket.getInputStream());
         ObjectOutputStream serverOutputStream = new ObjectOutputStream(socket.getOutputStream());
-        Command command;
 
 
         while(true) {
@@ -81,8 +39,8 @@ public class ServerAutomat extends IOException {
             String commandFromInput = split[0].trim();
             String data = split[1].trim();
 
-            String s = existingCommand(commandFromInput, data);
-            serverOutputStream.writeObject(s);
+            String info = transit.existingCommand(commandFromInput, data);
+            serverOutputStream.writeObject(info);
 
         }
     }
